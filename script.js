974b9ea11f2a77a7e44d746b409ed22aa740d9b7
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2024-01-09 23:04:31 -0300

    efeito de texto no site

---
+++ script.js
@@ -0,0 +1,18 @@
+window.sr = ScrollReveal({ reset: true});
+
+
+ScrollReveal().reveal('.titulo');
+ScrollReveal().reveal('.presencial');
+
+
+
+sr.reveal('.titulo', {duration:2500})
+sr.reveal('.presencial', {
+rotate: {x: 0, y: 80, z:0},
+duration: 2000
+})
+
+
+
+
+
