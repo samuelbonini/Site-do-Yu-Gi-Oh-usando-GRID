d8f1ed1fbb063ab56c492a40606753b85a9d8767
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2024-08-28 14:23:48 -0300

    Correções site yugioh grid

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 window.sr = ScrollReveal({ reset: true});
 
-
 ScrollReveal().reveal('.titulo');
 ScrollReveal().reveal('.presencial');
-
-
 
 sr.reveal('.titulo', {duration:2000})
 sr.reveal('.presencial', {
 rotate: {x: 0, y: 80, z:0},
 duration: 1500
 })
+
+sr.reveal('.efeitojs', {
+rotate: {x: 0, y: 80, z:0},
+duration: 1500
+})
+
 
 
 
